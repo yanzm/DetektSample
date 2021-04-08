de350dd823f5ae45369f0b7544e832168fc387b2
Yuki Anzai <anzai.y.aa@gmail.com>
2021-04-09 08:39:24 +0900

    Initial commmit

---
+++ app/src/main/java/net/yanzm/detektsample/MainActivity.kt
@@ -0,0 +1,30 @@
+package net.yanzm.detektsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println(find3rd(listOf("a", "b", "c", "d", "e"))) // c
+    }
+
+    fun find3rd(list: List<String>): String {
+        if (list.isEmpty()) {
+            return ""
+        }
+
+        if (list.size < THIRD_INDEX) {
+            return ""
+        }
+
+        // TODO:
+
+        return list[2]
+    }
+}
+
+private const val THIRD_INDEX = 3
